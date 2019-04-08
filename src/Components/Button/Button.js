61f837f3e8b8976dc276e399dde5fdb68017d469
Charles Hatch <charlesghatch@gmail.com>
2019-04-08 14:48:46 -0600

    added button and nav component folder, installed packages

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './button.css';
+
+export default function Button(props){
+    return <div className='button_wrapper'>
+    <button type='' className=''> Make Money</button>
+    </div> 
+}
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Nav(props) {
+  return <p>NAV</p>;
+}
