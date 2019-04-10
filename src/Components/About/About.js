a14a348f18ce93491f1436bac649c6943cbc00b5
Charles Hatch <charlesghatch@gmail.com>
2019-04-10 15:26:27 -0600

    added Products page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Content from '../Content/Content';
+
 export default function About(props){
     return (
         <div>
             <h1>About</h1>
+            <Content/>
         </div> 
     )
 };
