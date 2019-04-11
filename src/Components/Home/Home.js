7c39ca8d516f6606759f454e409c5bf98fc56504
Charles Hatch <charlesghatch@gmail.com>
2019-04-11 10:41:56 -0600

    restyled the title, added home content

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,16 @@
 import React from "react";
-
-import './Home.css';
+import "../../App.css";
+import "./Home.css";
 
 import Content from "../Content/Content";
 import Button from "../Button/Button";
 
 export default function Home(props) {
   return (
-    <div className='home_wrapper' >
-      <h1>Home</h1>
-      <Content />
+    <div className="home_wrapper">
+      <h1 className="title">Main Selling Point</h1>
+      <p className='large_body'>This is the information on why you want to click the link below. After reading this you wont be able to refuse</p>
       <Button />
-      <Content />
     </div>
   );
 }
