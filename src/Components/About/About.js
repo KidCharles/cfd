b18d5d660b2699fe421fc85561b935d9ce958ad3
Charles Hatch <charlesghatch@gmail.com>
2019-04-11 10:04:31 -0600

    styled title for mobile and desktop, as well as text, added link to drawer

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,15 +10,17 @@ export default function About(props) {
       <img src={banner} alt="CareFree Direct Banner" className="cfd_banner" />
       <div className='text_block'>
         <h1 className="title">Why CareFree Direct?</h1>
-        <p className="body">
+        <p className="large_body">
           We partner with Provider Organizations and Insurance Carriers to
           provide world class service to their members and prospects.
         </p>
-        <p className="body">
+        <br/>
+        <p className="large_body">
           We specialize in not only SELLING, but RETAINING members through
           direct and ongoing communication
         </p>
-        <p className='body'>
+        <br/>
+        <p className='large_body'>
           We excel in exceeding clients goals by understanding objectives, and
           pivoting real-time to drive superior outcomes
         </p>
--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -1,18 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./toolbar.css";
 
 const toolbar = props => {
-    let toolbarClasses ='toolbar';
-    if (props.show){
-        toolbarClasses = 'toolbar open';
-    }
+  let toolbarClasses = "toolbar";
+  if (props.show) {
+    toolbarClasses = "toolbar open";
+  }
   return (
     <nav className={toolbarClasses}>
       <ul>
-        <li className="">About</li>
+        <Link to="/About">
+          <li className="">About</li>
+        </Link>
         <li className="">Dental</li>
         <li className="">Medicare</li>
+        <Link to="/">
+          <li className="">Home</li>
+        </Link>
       </ul>
     </nav>
   );
