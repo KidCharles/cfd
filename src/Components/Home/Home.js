79aa047010b8a989c94a76b8e180897f23b484ac
Charles Hatch <charlesghatch@gmail.com>
2019-04-11 11:14:12 -0600

    styled the call cta component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../App.css";
 import "./Home.css";
+import Call from "../CallCTA/Callcta";
 
 import Content from "../Content/Content";
 import Button from "../Button/Button";
@@ -9,8 +10,13 @@ export default function Home(props) {
   return (
     <div className="home_wrapper">
       <h1 className="title">Main Selling Point</h1>
-      <p className='large_body'>This is the information on why you want to click the link below. After reading this you wont be able to refuse</p>
+      <p className="large_body">
+        This is the information on why you want to click the link below. After
+        reading this you wont be able to refuse
+      </p>
       <Button />
+      <div className="whiteSpace48" />
+      <Call />
     </div>
   );
 }
