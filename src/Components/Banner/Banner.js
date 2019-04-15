6d859e821c41bed5af587f814b86a7d531f697eb
Charles Hatch <charlesghatch@gmail.com>
2019-04-15 14:35:50 -0600

    added background image to banner component, and the text with styling for desktop

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 import "./Banner.css";
+import Button from "../Button/Button";
 
 export default function Banner(props) {
-  return <div className="bannerWrapper">banner</div>;
+  return (
+    <div className="bannerWrapper">
+      <h1 className="bannerTitle">Ever wanted Dental?</h1>
+      <p className="bannerText">
+        Join the 5 million+ satisfied customers who found health insurance
+        coverage
+      </p>
+      {/* <div className='whiteSpace16' ></div>  */}
+      <Button />
+    </div>
+  );
 }
--- src/Components/Medicare/Medicare.js
+++ src/Components/Medicare/Medicare.js
@@ -3,6 +3,7 @@ import "../../Components/About/About.css";
 import "../../App.css";
 
 import banner from "../../assets/logos/cfd_banner_left.png";
+import Call from "../CallCTA/Callcta";
 
 export default function Medicare(props) {
   return (
@@ -25,6 +26,7 @@ export default function Medicare(props) {
           pivoting real-time to drive superior outcomes
         </p>
         <div className="whiteSpace64" />
+        <Call/>
       </div>
     </div>
   );
