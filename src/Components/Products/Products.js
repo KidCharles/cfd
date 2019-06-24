47a08c09cf7a61a215849bf253ca345175edbb53
Charles Hatch <charlesghatch@gmail.com>
2019-06-24 12:46:26 -0600

    changed the home route to products

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,9 +1,34 @@
 import React from "react";
+import "../Home/Home.css";
+import "../../App.css";
+import Banner from "..//Banner/Banner";
 
 export default function Products(props) {
   return (
-    <div>
-      <h1>Products</h1>
+    <div className="home_wrapper">
+      <Banner
+        title={"CAREFREE DIRECT"}
+        text={
+          "We work hard to find the right health care solutions, so you don’t have to."
+        }
+        pic1={"pic1"}
+      />
+      <div className="text_container">
+        <h1 className="title">PRODUCTS</h1>
+        <p className="quote">"Health CARE, not just health insurance."</p>
+        <p className="large_body">
+          Carefree Direct is a leading healthcare insurance agency. Our Brokers
+          empower consumers to confidently make sound decisions with ease
+          regarding their health insurance options while eliminating unnecessary
+          difficulties. With nearly 4,000 new members a month trusting Carefree
+          to guide them in their healthcare decisions, we have quickly become
+          one of the fastest growing agencies in America. Understanding each
+          member’s unique situation, providing a unique solution for protection
+          and bringing value to their life is a core value for Carefree and each
+          of its brokers.
+        </p>
+      </div>
+      <div className="whiteSpace64" />
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,11 @@ import About from "./Components/About/About";
 import Dental from './Components/Dental/Dental';
 import Vision from './Components/Vision/Vision';
 import Medicare from './Components/Medicare/Medicare';
+import Products from './Components/Products/Products';
 
 export default (
   <Switch>
-    <Route exact path="/" component={Home} />
+    <Route exact path="/" component={Products} />
     <Route path="/About" component={About} />
     <Route path="/dental" component={Dental} />
     <Route path="/vision" component={Vision} />
